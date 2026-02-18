ce24451d5439b3d143de878d47ed052e356aa792
Diwakar Rajak <amanrajak049@gmail.com>
2026-02-18 19:46:46 +0530

    first commit

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+   css: {
+    postcss: './postcss.config.cjs',
+   },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  rules: {
+    "react/prop-types": 0,
+  },
+});
